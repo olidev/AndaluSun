18fc9086e5f204e824876a458a20612b7ae6ee6c
olidev <olivier@devue.be>
2014-04-13 14:11:50 +0200

    ajout icônes

--- js/AndaluSun.js
+++ js/AndaluSun.js
@@ -1,44 +1,13 @@
 
-	
 	var myScroll;
 	function loaded() {
 		myScroll = new iScroll('wrapper');
 	}
 	
 	document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
-	
-	/* * * * * * * *
-	 *
-	 * Use this for high compatibility (iDevice + Android)
-	 *
-	 */
-	document.addEventListener('DOMContentLoaded', function () { setTimeout(loaded, 200); }, false);
-	/*
-	 * * * * * * * */
-	
-	
-	/* * * * * * * *
-	 *
-	 * Use this for iDevice only
-	 *
-	 */
-	//document.addEventListener('DOMContentLoaded', loaded, false);
-	/*
-	 * * * * * * * */
-	
-	
-	/* * * * * * * *
-	 *
-	 * Use this if nothing else works
-	 *
-	 */
-	//window.addEventListener('load', setTimeout(function () { loaded(); }, 200), false);
-	/*
-	 * * * * * * * */
-	
-	</script>
 
-    <script type="text/javascript" charset="utf-8">
+	document.addEventListener('DOMContentLoaded', function () { setTimeout(loaded, 200); }, false);
+
 
     // Attendre que PhoneGap soit prêt
     //
